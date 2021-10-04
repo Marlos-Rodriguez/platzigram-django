0490a77c4e82ee9dc6db98a83d58bffa50b206f9
Marlos Rodriguez <mrodriguezp@ext.bancoripley.com>
2021-10-04 18:07:21 -0300

    Sign up

--- users/views.py
+++ users/views.py
@@ -3,6 +3,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Erros Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+
+# Local Apps
+from users.models import Profile
+
 # Create your views here.
 
 
@@ -11,12 +20,44 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect('feed')
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username or password'})
     return render(request, 'users/login.html')
+
+
+def singup_view(request):
+    """Sign Up view"""
+    if request.method == 'POST':
+        # Get User data
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        # Create user or return error
+        try:
+            user = User.objects.create_user(
+                username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        # Add aditional Info
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        # Create Profile from user
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+    return render(request, 'users/signup.html')
 
 
 @login_required
